0fc8f6ee58043fb41726e0d4e7990f9dedc23c53
brent couillard <brentcouillard@yahoo.com>
2023-01-05 02:03:49 -0600

    commit tree 4711cac226f226f8954360b76575162d108386f7

--- main.js
+++ main.js
@@ -2,8 +2,43 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// My Code Below
 
+document.addEventListener("DOMContentLoaded", () => {
+    clickListener()
+    
+})
+
+
+
+function clickListener() {
+  document.addEventListener("click", (event) => {
+    if(event.target.classList[0] === "like-glyph"){
+      mimicServerCall()
+        .then(resp => {
+          const activated = event.target.classList.contains("activated-heart");
+          if(activated){
+            event.target.classList.remove("activated-heart")
+            event.target.innerHTML = EMPTY_HEART
+          }else{
+            event.target.classList.add("activated-heart")
+            event.target.innerHTML = FULL_HEART
+          }
+            
+        })
+        .catch(error => {
+          errorModal.classList.remove("hidden")
+          setTimeout(() => {
+            errorModal.classList.add("hidden")
+          }, 3000)
+        })
+    }
+  })
+}
+
+
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add("hidden")
 
 
 
